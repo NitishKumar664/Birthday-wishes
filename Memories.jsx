24a4af714aa0c30c3f06aee4831e9c5dfa9c1b92
Nitish Kumar <89963100+NitishKumar664@users.noreply.github.com>
2026-07-19 22:34:00 +0530

    Add Memories component with image and button

---
+++ Memories.jsx
@@ -0,0 +1,9 @@
+export default function Memories({ next }) {
+  return (
+    <div>
+      <h1>Cute Memories 💕</h1>
+      <img src="/images/pic1.jpg" className="w-64 mx-auto"/>
+      <button onClick={next}>Next 🎁</button>
+    </div>
+  );
+}
