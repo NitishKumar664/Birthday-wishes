c614db7dd2c26c9a4ca3398e6ab0cc75034c393a
Nitish Kumar <89963100+NitishKumar664@users.noreply.github.com>
2026-07-19 22:32:29 +0530

    Add Stats component for birthday celebration

---
+++ Stats.jsx
@@ -0,0 +1,9 @@
+export default function Stats({ next }) {
+  return (
+    <div>
+      <h1>Happy Birthday 🎂</h1>
+      <p>20 Years • 7304 Days</p>
+      <button onClick={next}>See Memories 💞</button>
+    </div>
+  );
+}
