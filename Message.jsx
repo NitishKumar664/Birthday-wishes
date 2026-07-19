1871bbe71a705ac769a33da823f84006943e83c3
Nitish Kumar <89963100+NitishKumar664@users.noreply.github.com>
2026-07-19 22:31:17 +0530

    Add Message component with a continue button

---
+++ Message.jsx
@@ -0,0 +1,8 @@
+export default function Message({ next }) {
+  return (
+    <div>
+      <h1>There's something special I want to tell you...</h1>
+      <button onClick={next}>Continue 💖</button>
+    </div>
+  );
+}
